090225210835b78461148b878bb1199cfe0ee569
alexbryw <alexbryw@gmail.com>
2020-03-23 15:50:46 +0100

    added new empty components.

---
+++ src/components/ShoppingCart.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function ShoppingCart() {
+
+    return (
+        <h1>text</h1>
+    );
+}
---
+++ src/components/checkout/CheckOut.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function CheckOut() {
+
+    return (
+        <h1>text</h1>
+    );
+}
