6ae42aa946ace8e5661856aab6d4e3863eb1aa5c
Oskar Lindblad <lindbladoskar@gmail.com>
2020-03-24 09:35:58 +0100

    basic layout home

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, {CSSProperties} from 'react'
+import ProductCard from './ProductCard'
+import { item, itemResponse } from '../items/itemList'
+import Grid from '@material-ui/core/Grid';
 
 export default function Home() {
-
+    console.log(item)
     return (
-        <h1>Home</h1>
-    );
+        <div style={homeContainer}
+        >
+            <Grid 
+                container
+                spacing={3}
+                direction="row"
+                justify="center"
+                alignItems="center"
+            >
+                {item.map((itemData:any, index:number) =>
+                <Grid key={index}
+                item xs={12} sm={6} md={4}
+                >
+                    <ProductCard itemData={itemData}/>
+                </Grid>
+                )}
+            </Grid>
+        </div>
+    )
 }
+
+const homeContainer: CSSProperties = {
+    marginLeft: 'auto',
+    marginRight: 'auto'
+  }
--- src/components/home/ProductCard.tsx
+++ src/components/home/ProductCard.tsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import { itemResponse } from '../items/itemList'
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import PurchaseButtons from './PurchaseButtons'
 
-export default function ProductCard() {
-
-    return (
-        <h1>text</h1>
-    );
+interface Props{
+ itemData: itemResponse
 }
+
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 200,
+    },
+  });
+  
+  export default function ProductCard(props : Props) {
+    const classes = useStyles();
+  
+    return (
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image={props.itemData.imgURL}
+            title={props.itemData.imgURL + " Image"}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {props.itemData.name}
+            </Typography>
+            <PurchaseButtons itemPrice={props.itemData.price}/>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    );
+  }
