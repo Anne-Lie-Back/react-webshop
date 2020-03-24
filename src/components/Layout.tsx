e6d53815a92a162bc7123257e30e4536df72dd22
alexbryw <alexbryw@gmail.com>
2020-03-24 21:34:02 +0100

    Added withRoute to Product and rout match interface in typings.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,23 +4,35 @@ import Home from './home/Home';
 import Footer from './Footer';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import CheckOut from './checkout/CheckOut';
+import Product from './product/Product';
 
-export default function Layout() {
-
-    return (
-        <BrowserRouter>
-        <div>
-            <Header/>
-                <Switch>
-                    <Route path="/checkout">
-                        <CheckOut/>
-                    </Route>
-                    <Route exact path="/">
-                        <Home/>
-                    </Route>
-                </Switch>
-            <Footer/>
-        </div>
-        </BrowserRouter>
-    );
+interface Props{}
+interface State{}
+export default class Layout extends React.Component<Props, State> {
+    render(){
+        console.log("from layout")
+        console.log(this.props)
+         return (
+            <BrowserRouter>
+            <div>
+                <Header/>
+                    <Switch>
+                        <Route path="/checkout">
+                            <CheckOut/>
+                        </Route>
+                        <Route exact path="/product">
+                            <div><h2>Select a product</h2></div>
+                        </Route>
+                        <Route path="/product/:id">
+                            <Product/>
+                        </Route> 
+                        <Route exact path="/">
+                            <Home/>
+                        </Route>
+                    </Switch>
+                <Footer/>
+            </div>
+            </BrowserRouter>
+        );   
+    }
 }
