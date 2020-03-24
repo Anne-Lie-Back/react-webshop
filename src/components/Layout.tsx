4c0e1393a38ca5d8c5edabf8f485e01f658e9ebe
alexbryw <alexbryw@gmail.com>
2020-03-24 14:39:42 +0100

    Installed react-router-dom and @types. Added links in Header

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,14 +2,25 @@ import React from 'react'
 import Header from './Header';
 import Home from './home/Home';
 import Footer from './Footer';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CheckOut from './checkout/CheckOut';
 
 export default function Layout() {
 
     return (
+        <BrowserRouter>
         <div>
             <Header/>
-            <Home/>
+                <Switch>
+                    <Route path="/checkout">
+                        <CheckOut/>
+                    </Route>
+                    <Route exact path="/">
+                        <Home/>
+                    </Route>
+                </Switch>
             <Footer/>
         </div>
+        </BrowserRouter>
     );
 }
