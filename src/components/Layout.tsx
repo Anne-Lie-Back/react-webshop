2bb59c63bc71b66298bc1041d1860474bf54c29e
alexbryw <alexbryw@gmail.com>
2020-03-25 00:19:07 +0100

    Product routing, pages, and route typing works.
    run 'npm install' first, needed for router-dom.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,14 +4,12 @@ import Home from './home/Home';
 import Footer from './Footer';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import CheckOut from './checkout/CheckOut';
-import Product from './product/Product';
+import ProductPage from './product/ProductPage';
 
 interface Props{}
 interface State{}
 export default class Layout extends React.Component<Props, State> {
     render(){
-        console.log("from layout")
-        console.log(this.props)
          return (
             <BrowserRouter>
             <div>
@@ -24,7 +22,7 @@ export default class Layout extends React.Component<Props, State> {
                             <div><h2>Select a product</h2></div>
                         </Route>
                         <Route path="/product/:id">
-                            <Product/>
+                            <ProductPage/>
                         </Route> 
                         <Route exact path="/">
                             <Home/>
