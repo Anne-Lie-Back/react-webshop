bdfa188f676b082518754d440957515c36f89e9a
Anne-Lie Bäck <56996804+Anne-Lie-Back@users.noreply.github.com>
2020-03-25 19:49:28 +0100

    so inputvalues now displays, but takes lot of code

--- src/components/checkout/Address.tsx
+++ src/components/checkout/Address.tsx
@@ -7,6 +7,12 @@ interface Props{
     nextStep: () => void
     name: string
     onChangeName: (event:any) => void
+    address: string
+    onChangeAddress: (event:any) => void
+    zipCode: number
+    onChangeZipCode: (event:any) => void
+    city: string
+    onChangeCity: (event:any) => void
 }
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -30,7 +36,10 @@ export default function BasicTextFields(props:Props) {
   return (
     <>
         <form className={classes.root} noValidate autoComplete="off">
-        <TextField id="standard-basic" label="namn" value={props.name} onChange = {props.onChangeName}/>
+        <TextField id="standard-basic" label="Namn" value={props.name} onChange = {props.onChangeName}/>
+        <TextField id="standard-basic" label="Adress" value={props.address} onChange = {props.onChangeAddress}/>
+        <TextField id="standard-basic" label="Postnummer" value={props.zipCode} onChange = {props.onChangeZipCode}/>
+        <TextField id="standard-basic" label="Ort" value={props.city} onChange = {props.onChangeCity}/>
         </form>
         <Button variant="contained" 
           color="primary"
--- src/components/checkout/CheckOut.tsx
+++ src/components/checkout/CheckOut.tsx
@@ -5,7 +5,10 @@ interface Props{
 }
 interface State{
     step:number,
-    name:string
+    name:string,
+    address: string,
+    zipCode: number,
+    city: string
 }
 
 export default class CheckOut extends React.Component<Props, State>{
@@ -14,7 +17,9 @@ export default class CheckOut extends React.Component<Props, State>{
         this.state = {
             step: 1,
             name: '',
-            
+            address: '',
+            zipCode: 0,
+            city: ''
         }   
     }
 
@@ -33,6 +38,11 @@ export default class CheckOut extends React.Component<Props, State>{
       }
 
       handleNameInput = (event: { target: { value: any } }) => this.setState({name:event.target.value})
+      handleAddressInput = (event: { target: { value: any } }) => this.setState({address:event.target.value})
+      handleZipCodeInput = (event: { target: { value: any } }) => this.setState({zipCode:event.target.value})
+      handleCityInput = (event: { target: { value: any } }) => this.setState({city:event.target.value})
+       
+    
 
     render(){
         const { step } = this.state
@@ -44,12 +54,22 @@ export default class CheckOut extends React.Component<Props, State>{
                         nextStep = {this.nextStep}
                         name = {this.state.name}
                         onChangeName = {this.handleNameInput}
+                        address = {this.state.address}
+                        onChangeAddress = {this.handleAddressInput}
+                        zipCode = {this.state.zipCode}
+                        onChangeZipCode = {this.handleZipCodeInput}
+                        city = {this.state.city}
+                        onChangeCity = {this.handleCityInput}
                         
                     />
                 )
             case 2:
                 return(
-                    <h1>How you wanna pay?</h1>
+                    <>
+                        <h2>{this.state.name}</h2>
+                        <p>{this.state.address}</p>
+                        <p>{this.state.zipCode}{this.state.city}</p>
+                    </>
                 )
         }
     }
