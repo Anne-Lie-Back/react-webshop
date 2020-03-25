cfbdd1e7bd8e7bc2ae89f9ca8f0e905a03304a49
Anne-Lie Bäck <56996804+Anne-Lie-Back@users.noreply.github.com>
2020-03-25 19:32:10 +0100

    formish

--- src/components/checkout/Address.tsx
+++ src/components/checkout/Address.tsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
-export default function Address() {
+interface Props{
+    nextStep: () => void
+    name: string
+    onChangeName: (event:any) => void
+}
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    },
+  }),
+);
 
-    return (
-        <h1>text</h1>
-    );
+export default function BasicTextFields(props:Props) {
+  const classes = useStyles();
+
+  const next = (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+    props.nextStep();
+  }
+
+  return (
+    <>
+        <form className={classes.root} noValidate autoComplete="off">
+        <TextField id="standard-basic" label="namn" value={props.name} onChange = {props.onChangeName}/>
+        </form>
+        <Button variant="contained" 
+          color="primary"
+          onClick = {next}> Fortsätt </Button>
+    </>
+  );
 }
--- src/components/checkout/CheckOut.tsx
+++ src/components/checkout/CheckOut.tsx
@@ -1,8 +1,56 @@
 import React from 'react'
+import Address from './Address'
 
-export default function CheckOut() {
+interface Props{
+}
+interface State{
+    step:number,
+    name:string
+}
 
-    return (
-        <h1>checkout</h1>
-    );
+export default class CheckOut extends React.Component<Props, State>{
+    constructor(props:Props){
+        super(props)
+        this.state = {
+            step: 1,
+            name: '',
+            
+        }   
+    }
+
+    nextStep = () => {
+        const { step } = this.state;
+        this.setState({
+          step: step + 1
+        })
+      }
+
+      previousStep = () => {
+        const { step } = this.state;
+        this.setState({
+          step: step - 1
+        })
+      }
+
+      handleNameInput = (event: { target: { value: any } }) => this.setState({name:event.target.value})
+
+    render(){
+        const { step } = this.state
+        console.log(this.state.name)
+        switch(step){
+            case 1:
+                return(
+                    <Address 
+                        nextStep = {this.nextStep}
+                        name = {this.state.name}
+                        onChangeName = {this.handleNameInput}
+                        
+                    />
+                )
+            case 2:
+                return(
+                    <h1>How you wanna pay?</h1>
+                )
+        }
+    }
 }
