fc4c7451afd3a434d1ecc89862981ac046567588
Anne-Lie Bäck <56996804+Anne-Lie-Back@users.noreply.github.com>
2020-03-26 11:01:24 +0100

    address fields is functional

--- src/components/checkout/Address.tsx
+++ src/components/checkout/Address.tsx
@@ -5,14 +5,20 @@ import Button from '@material-ui/core/Button';
 
 interface Props{
     nextStep: () => void
-    name: string
-    onChangeName: (event:any) => void
+    firstName: string
+    onChangeFirstName: (event:any) => void
+    lastName: string
+    onChangeLastName: (event:any) => void
     address: string
     onChangeAddress: (event:any) => void
-    zipCode: number
+    zipCode: any
     onChangeZipCode: (event:any) => void
     city: string
     onChangeCity: (event:any) => void
+    eMail: string
+    onChangeEMail: (event:any) => void
+    mobNr: any
+    onChangeMobNr: (event:any) => void
 }
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -36,10 +42,16 @@ export default function BasicTextFields(props:Props) {
   return (
     <>
         <form className={classes.root} noValidate autoComplete="off">
-        <TextField id="standard-basic" label="Namn" value={props.name} onChange = {props.onChangeName}/>
-        <TextField id="standard-basic" label="Adress" value={props.address} onChange = {props.onChangeAddress}/>
-        <TextField id="standard-basic" label="Postnummer" value={props.zipCode} onChange = {props.onChangeZipCode}/>
-        <TextField id="standard-basic" label="Ort" value={props.city} onChange = {props.onChangeCity}/>
+          <TextField id="standard-basic" label="Förnamn" value={props.firstName} onChange = {props.onChangeFirstName}/>
+          <TextField id="standard-basic" label="Efternamn" value={props.lastName} onChange = {props.onChangeLastName}/>
+          <br/>
+          <TextField id="standard-basic" label="Adress" value={props.address} onChange = {props.onChangeAddress}/>
+          <br/>
+          <TextField id="standard-basic" label="Postnummer" value={props.zipCode} onChange = {props.onChangeZipCode}/>
+          <TextField id="standard-basic" label="Ort" value={props.city} onChange = {props.onChangeCity}/>
+          <br/>
+          <TextField id="standard-basic" label="E-Mail" value={props.eMail} onChange = {props.onChangeEMail}/>
+          <TextField id="standard-basic" label="Mobile" value={props.mobNr} onChange = {props.onChangeMobNr}/>
         </form>
         <Button variant="contained" 
           color="primary"
--- src/components/checkout/CheckOut.tsx
+++ src/components/checkout/CheckOut.tsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, {CSSProperties} from 'react'
 import Address from './Address'
+import Shipping from './Shipping'
+import Button from '@material-ui/core/Button';
 
 interface Props{
 }
 interface State{
     step:number,
-    name:string,
+    firstName:string,
+    lastName:string,
     address: string,
-    zipCode: number,
+    zipCode: any,
     city: string
+    eMail: string,
+    mobNr:any
 }
 
 export default class CheckOut extends React.Component<Props, State>{
@@ -16,10 +21,13 @@ export default class CheckOut extends React.Component<Props, State>{
         super(props)
         this.state = {
             step: 1,
-            name: '',
+            firstName: '',
+            lastName: '',
             address: '',
-            zipCode: 0,
-            city: ''
+            zipCode: '',
+            city: '',
+            eMail:'',
+            mobNr: '',
         }   
     }
 
@@ -37,40 +45,68 @@ export default class CheckOut extends React.Component<Props, State>{
         })
       }
 
-      handleNameInput = (event: { target: { value: any } }) => this.setState({name:event.target.value})
+      handleFirstNameInput = (event: { target: { value: any } }) => this.setState({firstName:event.target.value})
+      handleLastNameInput = (event: { target: { value: any } }) => this.setState({lastName:event.target.value})
       handleAddressInput = (event: { target: { value: any } }) => this.setState({address:event.target.value})
       handleZipCodeInput = (event: { target: { value: any } }) => this.setState({zipCode:event.target.value})
       handleCityInput = (event: { target: { value: any } }) => this.setState({city:event.target.value})
+      handleEMailInput = (event: { target: { value: any } }) => this.setState({eMail:event.target.value})
+      handleMobNrInput = (event: { target: { value: any } }) => this.setState({mobNr:event.target.value})
        
     
 
     render(){
         const { step } = this.state
-        console.log(this.state.name)
         switch(step){
             case 1:
                 return(
+                    <>
+                    <h2>Här är listan på allt du vill köpa! (eller kommer vara)</h2>
                     <Address 
                         nextStep = {this.nextStep}
-                        name = {this.state.name}
-                        onChangeName = {this.handleNameInput}
+                        firstName = {this.state.firstName}
+                        onChangeFirstName = {this.handleFirstNameInput}
+                        lastName = {this.state.lastName}
+                        onChangeLastName = {this.handleLastNameInput}
                         address = {this.state.address}
                         onChangeAddress = {this.handleAddressInput}
                         zipCode = {this.state.zipCode}
                         onChangeZipCode = {this.handleZipCodeInput}
                         city = {this.state.city}
-                        onChangeCity = {this.handleCityInput}
-                        
+                        onChangeCity = {this.handleCityInput}   
+                        eMail = {this.state.eMail}
+                        onChangeEMail = {this.handleEMailInput}
+                        mobNr = {this.state.mobNr}
+                        onChangeMobNr = {this.handleMobNrInput}        
                     />
+                    <Shipping/>
+                    </>
                 )
             case 2:
                 return(
-                    <>
-                        <h2>{this.state.name}</h2>
+                    <div style = {temporaryStyling}>
+                        <p>{this.state.firstName} {this.state.lastName}</p>
                         <p>{this.state.address}</p>
-                        <p>{this.state.zipCode}{this.state.city}</p>
-                    </>
+                        <p>{this.state.zipCode} {this.state.city}</p>
+                        <br/>
+                        <p>E-Mail: {this.state.eMail}</p>
+                        <p>Mobilnummer: {this.state.mobNr}</p>
+
+                        <br/>
+                        <h5>Funktionen att det stämmer finns inte än</h5>
+                        <Button variant="contained" 
+                            color="primary"
+                            onClick = {this.previousStep}> Stämmer inte?
+                        </Button>
+
+                    </div>
                 )
         }
     }
+}
+
+const temporaryStyling:CSSProperties ={
+    border: '2px solid blue',
+    margin: '3rem',
+    padding: '2rem'
 }
