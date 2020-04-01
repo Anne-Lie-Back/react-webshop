506b3e237aec9d14c11dbbccdf54b8cd96ec375f
Anne-Lie Bäck <56996804+Anne-Lie-Back@users.noreply.github.com>
2020-04-01 10:57:58 +0200

    moved payment and fixed minor but major bug

--- src/components/checkout/CheckOut.tsx
+++ src/components/checkout/CheckOut.tsx
@@ -130,10 +130,14 @@ export default class CheckOut extends React.Component<Props, State>{
                                 <p>Totalkostnad: {total} kr</p>
         
                                 <b/>
-                                <Button variant="contained" 
+                                <Payment
+                                onSubmit={this.onPaymentFormSubmit}
+                                customerInfo={this.state.customerInfo}
+                                />
+{/*                                 <Button variant="contained" 
                                     color="primary"
                                     onClick = {this.nextStep}> Stämmer?
-                                </Button>
+                                </Button> */}
                                 <Button variant="contained" 
                                     color="primary"
                                     onClick = {this.previousStep}> Stämmer inte?
@@ -145,22 +149,15 @@ export default class CheckOut extends React.Component<Props, State>{
                     )
                 }
                 break
-                case 3:
+/*                 case 3:
                 if(this.state.customerInfo) {
                     return(
                         <Container>
-                            <Payment
-                                onSubmit={this.onPaymentFormSubmit}
-                                customerInfo={this.state.customerInfo}
-                            />
-                            <Button variant="contained" 
-                            color="primary"
-                            onClick = {this.previousStep}> Tillbaka 
-                            </Button>
+
                         </Container>
                     )
                 }
-                break
+                break */
 
                 case 4:
                     if(this.state.customerInfo && this.state.customerPaymentInfo) {
