28d314035a06342e649d353d084d1437323db797
Oskar Lindblad <lindbladoskar@gmail.com>
2020-04-01 19:15:50 +0200

    admin class

--- src/components/admin/Admin.tsx
+++ src/components/admin/Admin.tsx
@@ -6,15 +6,40 @@ import Card from '@material-ui/core/Card'
 import ProductAdminList from './ProductAdminList'
 import RenameItem from './NewItemToggle'
 
-export default function Admin() {
-    return(
-        <Container>
-            {itemsLS.map((itemData:Product, index:number) =>
-                <ProductAdminList itemData={itemData} key={index} arrayIndex={index}/>
-            )}
-            <Card variant="outlined">
-                <RenameItem/>
-            </Card>
-        </Container>
-    )
+interface Props {
+}
+
+interface State {
+    userMassage: string,
+    id: number,
+    name: string, 
+    price: number,
+    imgURL: string,
+    description: string
+}
+
+export default class Admin extends React.Component<Props, State> {
+    constructor(props:Props){
+        super(props)
+        this.state = {
+            userMassage: "",
+            id: itemsLS[0].id,
+            name: itemsLS[0].name, 
+            price: itemsLS[0].price,
+            imgURL: itemsLS[0].imgURL,
+            description: itemsLS[0].description
+        }   
+    }
+    render(){
+        return(
+            <Container>
+                {itemsLS.map((itemData:Product, index:number) =>
+                    <ProductAdminList itemData={itemData} key={index} arrayIndex={index}/>
+                )}
+                <Card variant="outlined">
+                    <RenameItem/>
+                </Card>
+            </Container>
+        )
+    }
 }
