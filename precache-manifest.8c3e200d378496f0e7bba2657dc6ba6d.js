5b005201d1defa93ac5e976275ac2f6e0669e592
alexbryw <alexbryw@gmail.com>
2020-04-02 18:21:43 +0200

    Updates

---
+++ precache-manifest.8c3e200d378496f0e7bba2657dc6ba6d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0e9b5313a700aea347b34245efa27f6",
+    "url": "/react-webshop/index.html"
+  },
+  {
+    "revision": "fc42175d6fe66a6538e7",
+    "url": "/react-webshop/static/css/main.f31feee6.chunk.css"
+  },
+  {
+    "revision": "8b1ad222b5203d25b451",
+    "url": "/react-webshop/static/js/2.e7b2451f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-webshop/static/js/2.e7b2451f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc42175d6fe66a6538e7",
+    "url": "/react-webshop/static/js/main.4c8ea7e1.chunk.js"
+  },
+  {
+    "revision": "4f54b90de3cf3c7c625e",
+    "url": "/react-webshop/static/js/runtime-main.684b0842.js"
+  },
+  {
+    "revision": "ebff2ac1f087f4ef9c7572fd18873e29",
+    "url": "/react-webshop/static/media/klarna.ebff2ac1.png"
+  },
+  {
+    "revision": "30c19a33d1f6c59f160fa89505f828b4",
+    "url": "/react-webshop/static/media/logo.30c19a33.png"
+  },
+  {
+    "revision": "530bf4e0bf7cb64425edc241b024bfb4",
+    "url": "/react-webshop/static/media/mastercard.530bf4e0.png"
+  },
+  {
+    "revision": "14d6fc55637fa66bfa38e56479f303d0",
+    "url": "/react-webshop/static/media/sphere.14d6fc55.png"
+  },
+  {
+    "revision": "1d4efc37ddc73423f64c3a2455486f6f",
+    "url": "/react-webshop/static/media/swish.1d4efc37.png"
+  }
+]);
