83400376ea735c30fa45c2c8f38e4bb9de617298
alexbryw <alexbryw@gmail.com>
2020-04-03 11:54:57 +0200

    removed logs in CartContext

--- src/contexts/cartContext.tsx
+++ src/contexts/cartContext.tsx
@@ -42,13 +42,10 @@ export class CartProvider extends React.Component<Props, State>{
     addProduct = (inItemId: number, inNrItems: number) => {
         const cartListPosition = this.findItemInCart(inItemId)
         const updatedCartList = [...this.state.cartList]
-        console.log("start cart list")
-        console.log(this.state.cartList)
         
         if(cartListPosition !== false){
             updatedCartList[cartListPosition].nrItems = updatedCartList[cartListPosition].nrItems + inNrItems
             if(updatedCartList[cartListPosition].nrItems < 1){
-                console.log("negative item count, remove from list")
                 this.removeItemFromCart(inItemId)
             } else {
                 this.setState({
@@ -58,18 +55,13 @@ export class CartProvider extends React.Component<Props, State>{
                     savedCartTotalPrice: this.calcTotalCartPrice(updatedCartList),
                 })
             }
-            console.log("update cart item " )
-            console.log(updatedCartList)
         } else {
             if(inNrItems > 0){
                 const product = items.find(({id}) => id === inItemId)
                 if(product){
                     updatedCartList.push({id: inItemId, nrItems: inNrItems, product:product})
-                } else{
-                    console.log("product not found")
                 }
-                console.log("push new item to list " )
-                console.log(updatedCartList)
+
                 this.setState({
                     // cartList: [...this.state.cartList, {id: inItemId, nrItems: inNrItems}]
                     cartList: [...updatedCartList],
@@ -77,8 +69,6 @@ export class CartProvider extends React.Component<Props, State>{
                     savedCheckoutCartList: [...updatedCartList],
                     savedCartTotalPrice: this.calcTotalCartPrice(updatedCartList),
                 })
-            } else {
-                console.log("Cannot add zero or negative nr of items.")
             }
         }
     }
@@ -99,7 +89,6 @@ export class CartProvider extends React.Component<Props, State>{
         let cartListPosition = 0
         for (let i = 0; i < this.state.cartList.length; i++) {
             const item = this.state.cartList[i];
-            // console.log(item.id + " id and inId: " + InItemId)
             if(item.id === InItemId){
                 itemFound = true
                 cartListPosition = i
@@ -107,10 +96,8 @@ export class CartProvider extends React.Component<Props, State>{
         }
 
         if(itemFound){
-            // console.log("item array position " + cartListPosition)
             return cartListPosition
         } else {
-            // console.log("item not found.")
             return false
         }
 
@@ -121,15 +108,12 @@ export class CartProvider extends React.Component<Props, State>{
         let updatedCartList = [...this.state.cartList]
         if(cartListPosition !== false){
             updatedCartList.splice(cartListPosition,1)
-            console.log("item removed")
             this.setState({
                 cartList: [...updatedCartList],
                 cartTotalPrice: this.calcTotalCartPrice(updatedCartList),
                 savedCheckoutCartList: [...updatedCartList],
                 savedCartTotalPrice: this.calcTotalCartPrice(updatedCartList),
             })
-        } else {
-            console.log("item to remove not found.")
         }
     }
 
